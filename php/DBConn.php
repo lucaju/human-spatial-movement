88de78580bfc88d30a254da7260801e0f3c7b769
Luciano Frizzera <lucaju@me.com>
2015-06-15 09:35:05 -0700

    Update DBConn.php

--- php/DBConn.php
+++ php/DBConn.php
@@ -6,15 +6,9 @@ class DBConn {
 	
 	public static function getConnection() {
 
-		$db = "fluxoart_hsp";
-
-		//local
-		// $user = "root";
-		// $pass = "root";
-
-		//remore
-		$user = "fluxoart_movemen";
-		$pass = "HumanSpatialMovement";
+		$db = "db";
+		$user = "user";
+		$pass = "pass";
 	
 		//if I have no connection, build one
 		if (empty(self::$connection)) {
